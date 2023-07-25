bd005fa114a99247d93ccebae3d6011c4cd65f26
Jakub Pacewicz <pecet3107@gmail.coim>
2023-07-25 20:44:05 +0200

    test

--- next.config.js
+++ next.config.js
@@ -3,11 +3,14 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: "https",
-                hostname: "**",
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '**',
             },
         ],
     },
+
 }
 
 module.exports = nextConfig
