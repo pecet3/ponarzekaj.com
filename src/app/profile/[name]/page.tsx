055ae20e82460939b8fb70328de8b6807441df94
pecet3 <pecet3107@gmail.com>
2023-07-30 21:10:59 +0200

    implemented PostView

--- src/app/profile/[name]/page.tsx
+++ src/app/profile/[name]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { db } from "@/lib/db";
 import Error from "@/components/Error";
 import Image from "next/image";
+import { PostView } from "@/components/PostView";
 interface PageProps {
   params: {
     name: string;
@@ -16,18 +17,37 @@ const page = async ({ params }: PageProps) => {
       name: decodeURI(name)
     }
   })
-  if(data.length === 0)return <Error />
+
   const user = data[0]
+
+
+  const posts = await db.post.findMany({
+    where:{
+      authorId: user.id
+    },
+    include: {
+      comments: true,
+      author: true,
+    },
+  })
+
+  if(data.length === 0)return <Error />
+
   return <main className="flex flex-col min-h-screen m-auto mt-8 justify-start items-center">
-    <section className="rounded-md bg-indigo-400 p-1 sm:p-2 flex flex-col shadow-lg shadow-indigo-600 max-w-3xl">
+    <section className="rounded-md bg-indigo-400 flex flex-col shadow-lg shadow-indigo-600 max-w-3xl">
     <div className="relative">
-      <Image src={user.backgroundImage ?? ""} alt={`zdjęcie ${user.name}`} height={96} width={96} className="h-96   w-screen"/>
-    <Image src={user.image ?? ""} alt={`zdjęcie ${user.name}`} height={96} width={96} className="absolute rounded-full w-32 h-32 left-3 bottom-0  "/>
-  {/* <span className="flex justify-center flex-col">
-  <p className="text-2xl text-slate-800">{user.name}</p>
-  <p className="text-xl text-slate-600">{user.email}</p>
-  </span> */}
+      <Image src={user.backgroundImage ?? ""} alt={`zdjęcie ${user.name}`} height={96} width={96} className="h-96 rounded-t-md  w-screen"/>
+    <Image src={user.image ?? ""} alt={`zdjęcie ${user.name}`} height={256} width={256} className="absolute rounded-full w-40 h-40 left-3 bottom-[-5rem] "/>
     </div>
+    <span className="flex justify-center flex-col ml-48">
+      <p className="text-2xl text-slate-800">{user.name}</p>
+      <p className="text-xl text-slate-600">{user.email}</p>
+    </span>
+<div className="mt-10">
+  {posts.map((post)=>{
+    return <PostView post={post} key={post.id} />
+  })}
+</div>
     </section>
   </main>;
 };
