482cd8bb22235e4076c0997d66b907156202144e
pecet3 <pecet3107@gmail.com>
2023-07-30 19:46:20 +0200

    searching user

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { db } from "@/lib/db";
 interface PageProps {
   params: {
     id: string;
@@ -8,6 +8,12 @@ interface PageProps {
 
 const page = async ({ params }: PageProps) => {
   const { id } = params;
+  const data = await db.user.findUnique({
+    where:{
+      id: id
+    }
+  })
+  console.log(data)
   return <main className="flex flex-col background min-h-screen">{id}</main>;
 };
 
