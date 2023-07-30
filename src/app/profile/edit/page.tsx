ce11e2dd858de26f421ed6339e4de6f529f2761a
pecet3 <pecet3107@gmail.com>
2023-07-30 20:50:07 +0200

    updated tailwind

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -5,12 +5,12 @@ const page = async () => {
   const session = await getAuthSession();
   if (!session)
     return (
-      <p className="flex flex-col background min-h-screen">
+      <p className="flex flex-col  min-h-screen">
         Nie jesteś zalogowany, aby przeglądać profil
       </p>
     );
   return (
-    <main className="flex flex-col background min-h-screen">
+    <main className="flex flex-col min-h-screen">
       {session.user.name}
     </main>
   );
