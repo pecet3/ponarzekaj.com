3e2c4ffc4d37978726d8f29c261f34e84ac08650
pecet3 <pecet3107@gmail.com>
2023-07-31 22:33:28 +0200

    updated tailwind

--- src/app/profile/[name]/page.tsx
+++ src/app/profile/[name]/page.tsx
@@ -13,7 +13,7 @@ const page = async ({ params }: PageProps) => {
   const { name } = params;
 
   const data = await db.user.findMany({
-    where:{
+    where: {
       name: decodeURI(name)
     }
   })
@@ -22,7 +22,7 @@ const page = async ({ params }: PageProps) => {
 
 
   const posts = await db.post.findMany({
-    where:{
+    where: {
       authorId: user.id
     },
     include: {
@@ -31,23 +31,23 @@ const page = async ({ params }: PageProps) => {
     },
   })
 
-  if(data.length === 0)return <Error />
+  if (data.length === 0) return <Error />
 
-  return <main className="flex flex-col min-h-screen m-auto mt-8 justify-start items-center">
+  return <main className="flex flex-col min-h-screen m-auto mt-1 sm:mt-4 md:mt-8 justify-start items-center">
     <section className="rounded-md bg-indigo-400 flex flex-col shadow-lg shadow-indigo-600 max-w-3xl">
-    <div className="relative">
-      <Image src={user.backgroundImage ?? ""} alt={`zdjęcie ${user.name}`} height={96} width={96} className="h-96 rounded-t-md  w-screen"/>
-    <Image src={user.image ?? ""} alt={`zdjęcie ${user.name}`} height={256} width={256} className="absolute rounded-full w-40 h-40 left-3 bottom-[-5rem] "/>
-    </div>
-    <span className="flex justify-center flex-col ml-48">
-      <p className="text-2xl text-slate-800">{user.name}</p>
-      <p className="text-xl text-slate-600">{user.email}</p>
-    </span>
-<div className="mt-10">
-  {posts.map((post)=>{
-    return <PostView post={post} key={post.id} />
-  })}
-</div>
+      <div className="relative">
+        <Image src={user.backgroundImage ?? ""} alt={`zdjęcie ${user.name}`} height={96} width={96} className="h-72 sm:h-96 rounded-t-md  w-screen" />
+        <Image src={user.image ?? ""} alt={`zdjęcie ${user.name}`} height={256} width={256} className="absolute rounded-full w-40 h-40 left-3 bottom-[-5rem] " />
+      </div>
+      <span className="flex justify-center flex-col ml-48">
+        <p className="text-2xl text-slate-800">{user.name}</p>
+        <p className="text-xl text-slate-600">{user.email}</p>
+      </span>
+      <div className="mt-10 p-1">
+        {posts.map((post) => {
+          return <PostView post={post} key={post.id} />
+        })}
+      </div>
     </section>
   </main>;
 };
