d46fbbf1d7f2aead60931bc995ab794df1bad522
Jakub Pacewicz <pecet3107@gmail.coim>
2023-08-02 00:12:08 +0200

    error handling

--- src/app/api/post/create/route.ts
+++ src/app/api/post/create/route.ts
@@ -30,10 +30,10 @@ export async function POST(req: NextRequest) {
             }
         })
 
-
         return new Response("OK", { status: 200 })
     } catch (error) {
-
+        if (error instanceof z.ZodError) return new Response("Invalid payload", { status: 422 })
+        return new Response('Invalid request', { status: 400 })
     }
 
 
