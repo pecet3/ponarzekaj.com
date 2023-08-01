6015af7a563c175e9b51a3df3327c3df1a1e3c57
Jakub Pacewicz <pecet3107@gmail.coim>
2023-08-01 23:00:08 +0200

    new route

---
+++ src/app/api/post/create/route.ts
@@ -0,0 +1,31 @@
+
+import { NextResponse, NextRequest } from 'next/server'
+import { db } from '@/lib/db';
+import { z } from "zod"
+import { getAuthSession } from '@/lib/auth';
+
+export const createPostValidator = z.object({
+    content: z.string(),
+    authorId: z.string(),
+    emoji: z.string().emoji(),
+})
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json()
+
+        const { content, authorId, emoji } = createPostValidator.parse(body)
+
+        const session = getAuthSession()
+
+        if (!session) {
+            return new Response("Unauthorized", { status: 401 })
+        }
+
+    } catch (error) {
+
+    }
+
+
+    return NextResponse.json({ res })
+}
