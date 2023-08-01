b8fe7e9dd34610c8d5810f12957ae300f8a7e3da
Jakub Pacewicz <pecet3107@gmail.coim>
2023-08-02 00:17:33 +0200

    session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/lib/auth";
+import { getAuthSession } from "@/lib/auth";
 import axios from "axios";
 import { db } from "@/lib/db";
 import type { Post, Comment, User } from "@prisma/client";
@@ -10,7 +9,7 @@ import type { FullPost } from "@/types/prisma";
 import { CreatePost } from "../components/CreateAPost";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  const session = await getAuthSession();
 
   const posts = await db.post.findMany({
     include: {
@@ -30,7 +29,13 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center background">
       <section className="w-full max-w-3xl flex flex-col gap-2 sm:mt-2 rounded-md bg-indigo-400 bg-opacity-60 p-1 sm:p-2">
-        <CreatePost user={user} />
+        {session ? (
+          <CreatePost user={user} />
+        ) : (
+          <p className="text-sm text-center italic">
+            Tylko zalogowani użytkownicy mogą wstawiać posty
+          </p>
+        )}
         {posts.map((post) => {
           return <PostView key={post.id} post={post} />;
         })}
