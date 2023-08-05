15556a4fdfca706c868098785bd14276b546388a
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-05 21:41:48 +0200

    test

--- src/types/prisma.d.ts
+++ src/types/prisma.d.ts
@@ -1,9 +1,13 @@
 import type { Post, Comment, User, PostLikes, CommentLikes } from "@prisma/client";
 
-
+export type FullComment = Comment & {
+    author: User;
+    likes: CommentLikes[]
+}
 
 export type FullPost = Post & {
     comments: Comment[];
     author: User;
     likes: PostLikes[]
 };
+
