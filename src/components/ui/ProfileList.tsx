bcb37ab3fbf3ced9662fc8ec6aefdaaa05526f99
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-07 00:30:34 +0200

    new component

---
+++ src/components/ui/ProfileList.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import { FunctionComponent } from "react";
+
+export const ProfileList: FunctionComponent<{ username?: string | null }> = ({
+  username,
+}) => {
+  return (
+    <ul
+      tabIndex={0}
+      className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+    >
+      <li>
+        <Link href={`/profile/${username}`}>Profil</Link>
+      </li>
+      <li>
+        <a>Edytuj</a>
+      </li>
+      <li>
+        <button onClick={() => signOut()}>Wyloguj się</button>
+      </li>
+    </ul>
+  );
+};
