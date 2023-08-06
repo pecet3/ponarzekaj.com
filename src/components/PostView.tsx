d52d1f5c06746508f757b3dfc36dfb046437cc5a
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-06 19:14:59 +0200

    ui

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -14,7 +14,7 @@ interface Props {
 
 export const PostView: React.FC<Props> = ({ post }) => {
   return (
-    <div className="rounded-md bg-slate-700 hover:bg-slate-800 duration-300 md:hover:scale-[1.008] shadow-md shadow-slate-950 text-slate-200 w-full">
+    <div className="rounded-md bg-slate-700 hover:bg-slate-800 duration-300 md:hover:scale-[1.005] shadow-md shadow-slate-950 text-slate-200 w-full">
       <div className="flex items-end">
         <div className="flex justify-start gap-2 px-1 pt-1 md:px-2 md:pt-2">
           <Link href={`/profile/${post.author.name}`} className="">
