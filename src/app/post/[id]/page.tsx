b6e179806f57d7697abf9c26997d852feb9c21b1
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-06 16:29:22 +0200

    implemented MainTile

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { PostView } from "@/components/PostView";
 import { getAuthSession } from "@/lib/auth";
 import { CommentView } from "@/components/CommentView";
+import { MainTile } from "@/components/MainTile";
 interface PageProps {
   params: {
     id: string;
@@ -31,14 +32,32 @@ const page = async ({ params }: PageProps) => {
     },
   });
 
+  const post = await db.post.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      comments: true,
+      author: true,
+      likes: true,
+    },
+  });
   console.log(comments);
-
+  if (!post) return null;
   return (
-    <main>
+    <MainTile>
+      <PostView post={post} />
+      {session ? (
+        <p>create</p>
+      ) : (
+        <p className="text-sm text-center italic">
+          Tylko zalogowani użytkownicy mogą wstawiać posty
+        </p>
+      )}
       {comments.map((comment) => (
         <CommentView key={comment.id} comment={comment} />
       ))}
-    </main>
+    </MainTile>
   );
 };
 
