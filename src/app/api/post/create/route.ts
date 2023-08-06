e360911fcc1da4e4c8a04cb7e07861d8ac03302a
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-06 12:51:20 +0200

    edited path

--- src/app/api/post/create/route.ts
+++ src/app/api/post/create/route.ts
@@ -1,36 +1,36 @@
-
-import { NextRequest } from 'next/server'
-import { db } from '@/lib/db';
-import { createPostValidator } from '@/lib/validators';
-import { getAuthSession } from '@/lib/auth';
-import {z} from "zod"
+import { NextRequest } from "next/server";
+import { db } from "@/lib/db";
+import { createPostValidator } from "@/lib/validators";
+import { getAuthSession } from "@/lib/auth";
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
 
 export async function POST(req: NextRequest) {
-    try {
-        const body = await req.json()
+  try {
+    const body = await req.json();
 
-        const { content, authorId, emoji } = createPostValidator.parse(body)
+    const { content, authorId, emoji } = createPostValidator.parse(body);
 
-        const session = getAuthSession()
+    const session = getAuthSession();
 
-        if (!session) {
-            return new Response("Unauthorized", { status: 401 })
-        }
-
-        await db.post.create({
-            data: {
-                authorId,
-                emoji,
-                content,
-            }
-        })
-
-        return new Response("OK", { status: 200 })
-    } catch (error) {
-        if (error instanceof z.ZodError) return new Response("Invalid payload", { status: 422 })
-        return new Response('Invalid request', { status: 400 })
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
     }
 
+    await db.post.create({
+      data: {
+        authorId,
+        emoji,
+        content,
+      },
+    });
 
+    revalidatePath("/api/post/get-all");
 
+    return new Response("OK", { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return new Response("Invalid payload", { status: 422 });
+    return new Response("Invalid request", { status: 400 });
+  }
 }
