f17ea1b97c331bf77e352ae78e678de079cbcd9f
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-06 15:57:05 +0200

    commentView

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import Error from "@/components/Error";
 import Image from "next/image";
 import { PostView } from "@/components/PostView";
+import { getAuthSession } from "@/lib/auth";
+import { CommentView } from "@/components/CommentView";
 interface PageProps {
   params: {
     id: string;
@@ -10,8 +12,34 @@ interface PageProps {
 }
 
 const page = async ({ params }: PageProps) => {
+  const session = await getAuthSession();
+
+  const user = await db.user.findUnique({
+    where: {
+      id: session ? session?.user.id : "",
+    },
+  });
+
   const { id } = params;
-  return <main>{id}</main>;
+
+  const comments = await db.comment.findMany({
+    where: {
+      postId: id,
+    },
+    include: {
+      likes: true,
+    },
+  });
+
+  console.log(comments);
+
+  return (
+    <main>
+      {comments.map((comment) => (
+        <CommentView key={comment.id} comment={comment} />
+      ))}
+    </main>
+  );
 };
 
 export default page;
