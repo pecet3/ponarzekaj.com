208ca52249d6a8bd2e07f9cff96d713dcd4f7d2d
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-06 12:52:01 +0200

    new validator

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -1,7 +1,12 @@
-import {z} from "zod"
+import { z } from "zod";
 
 export const createPostValidator = z.object({
-    content: z.string(),
-    authorId: z.string(),
-    emoji: z.string().emoji(),
-})
+  content: z.string(),
+  authorId: z.string(),
+  emoji: z.string().emoji(),
+});
+
+export const addLikeValidator = z.object({
+  userId: z.string(),
+  postId: z.string(),
+});
