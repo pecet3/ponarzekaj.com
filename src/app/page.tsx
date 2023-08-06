12d5b6fd2091257ad7768ac2882145eb75c8435f
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-06 16:29:17 +0200

    implemented MainTile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import type { Post, Comment, User } from "@prisma/client";
 import { PostView } from "../components/PostView";
 import type { FullPost } from "@/types/prisma";
 import { CreatePost } from "../components/CreateAPost";
+import { MainTile } from "@/components/MainTile";
 
 export default async function Home() {
   const session = await getAuthSession();
@@ -27,19 +28,17 @@ export default async function Home() {
   });
 
   return (
-    <main className="flex min-h-screen flex-col items-center background">
-      <section className="w-full max-w-3xl flex flex-col gap-2 sm:mt-2 rounded-md bg-indigo-500 bg-opacity-60 p-1 sm:p-2">
-        {session ? (
-          <CreatePost user={user} />
-        ) : (
-          <p className="text-sm text-center italic">
-            Tylko zalogowani użytkownicy mogą wstawiać posty
-          </p>
-        )}
-        {posts.map((post) => {
-          return <PostView key={post.id} post={post} />;
-        })}
-      </section>
-    </main>
+    <MainTile>
+      {session ? (
+        <CreatePost user={user} />
+      ) : (
+        <p className="text-sm text-center italic">
+          Tylko zalogowani użytkownicy mogą wstawiać posty
+        </p>
+      )}
+      {posts.map((post) => {
+        return <PostView key={post.id} post={post} />;
+      })}
+    </MainTile>
   );
 }
