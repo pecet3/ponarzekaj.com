12752a989e4b7588eae4400fa3ec7e3cbfb89b47
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-06 15:57:57 +0200

    edited types

--- src/types/prisma.d.ts
+++ src/types/prisma.d.ts
@@ -1,14 +1,17 @@
-import type { Post, Comment, User, PostLikes, CommentLikes } from "@prisma/client";
+import type {
+  Post,
+  Comment,
+  User,
+  PostLikes,
+  CommentLikes,
+} from "@prisma/client";
 
 export type FullComment = Comment & {
-    author: User;
-    likes: CommentLikes[]
-}
-
-export type FullPost = Post & {
-    comments: Comment[];
-    author: User;
-    likes?: PostLikes[]
+  likes?: CommentLikes[];
 };
 
-
+export type FullPost = Post & {
+  comments: Comment[];
+  author: User;
+  likes?: PostLikes[];
+};
