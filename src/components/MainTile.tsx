f9ec411a1c62ec169976fc456b318b3572638369
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-06 16:29:09 +0200

    created MainTile

---
+++ src/components/MainTile.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const MainTile: FunctionComponent<Props> = ({ children }) => {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <section className="w-full max-w-3xl flex flex-col gap-2 sm:mt-2 rounded-md bg-indigo-500 bg-opacity-60 p-1 sm:p-2">
+        {children}
+      </section>
+    </main>
+  );
+};
