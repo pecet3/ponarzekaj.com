2db698ecf48f651b5ee73b58935fa23141be42d6
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-07 19:49:05 +0200

    new route

---
+++ src/app/api/post/delete-like/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest } from "next/server";
+import { db } from "@/lib/db";
+import { LikeValidator } from "@/lib/validators";
+import { getAuthSession } from "@/lib/auth";
+import { z } from "zod";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const { userId, postId } = LikeValidator.parse(body);
+
+    const session = getAuthSession();
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    await db.likePost.deleteMany({
+      where: {
+        AND: [
+          {
+            postId,
+          },
+          {
+            userId,
+          },
+        ],
+      },
+    });
+
+    return new Response("OK", { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return new Response("Invalid payload", { status: 422 });
+    return new Response("Invalid request", { status: 400 });
+  }
+}
