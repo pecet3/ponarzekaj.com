7906b776833396e0c7f6969746a1a2defd95c052
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-08 15:15:10 +0200

    new route

---
+++ src/app/api/post/delete/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest } from "next/server";
+import { db } from "@/lib/db";
+import { createPostValidator } from "@/lib/validators";
+import { getAuthSession } from "@/lib/auth";
+import { z } from "zod";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const { userId, postId } = createPostValidator.parse(body);
+
+    const session = getAuthSession();
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    await db.post.delete({
+      where: {
+        postId,
+      },
+    });
+
+    return new Response("OK", { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return new Response("Invalid payload", { status: 422 });
+    return new Response("Invalid request", { status: 400 });
+  }
+}
