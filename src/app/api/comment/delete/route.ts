530f0400fa72cb97626d151cd98dc19f52d7e76e
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-08 17:34:37 +0200

    new route

---
+++ src/app/api/comment/delete/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest } from "next/server";
+import { db } from "@/lib/db";
+import { commentValidator } from "@/lib/validators";
+import { getAuthSession } from "@/lib/auth";
+import { z } from "zod";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const { userId, commentId } = commentValidator.parse(body);
+
+    const session = await getAuthSession();
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    if (session.user.id !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    await db.comment.delete({
+      where: {
+        id: commentId,
+      },
+    });
+
+    return new Response("OK", { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return new Response("Invalid payload", { status: 422 });
+    return new Response("Invalid request", { status: 400 });
+  }
+}
