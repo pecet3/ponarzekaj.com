09fd29bc9358b38373208fe9c18f8b777787a9fd
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-09 22:25:03 +0200

    ui

--- src/components/MainTile.tsx
+++ src/components/MainTile.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 export const MainTile: FunctionComponent<Props> = ({ children }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <section className="w-full max-w-3xl flex flex-col gap-2 sm:mt-2 sm:rounded-md rounded-b-md bg-indigo-500 bg-opacity-60 p-1 sm:p-2">
+    <main className="flex min-h-screen flex-col items-center sm:my-2">
+      <section className="w-full max-w-3xl flex flex-col gap-2 sm:rounded-md rounded-b-md bg-indigo-500 bg-opacity-60 p-1 sm:p-2">
         {children}
       </section>
     </main>
