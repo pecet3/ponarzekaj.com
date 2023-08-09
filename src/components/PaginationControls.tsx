e9f2b5672824a1f7da2bbe940b8d7b355a25815b
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-09 22:24:57 +0200

    pagination controls

---
+++ src/components/PaginationControls.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { FC } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { BsCaretRightSquareFill, BsCaretLeftSquareFill } from "react-icons/bs";
+interface PaginationControlsProps {
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+}
+
+const PaginationControls: FC<PaginationControlsProps> = ({
+  hasNextPage,
+  hasPrevPage,
+}) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const page = searchParams.get("page") ?? "1";
+  const per_page = searchParams.get("per_page") ?? "10";
+
+  return (
+    <div className="flex gap-2 justify-center m-auto bg-fuchsia-900 bg-opacity-70 rounded-lg p-1">
+      <button
+        className="text-teal-500"
+        disabled={!hasPrevPage}
+        onClick={() => {
+          router.push(`/?page=${Number(page) - 1}&per_page=${per_page}`);
+        }}
+      >
+        <BsCaretLeftSquareFill size={24} />
+      </button>
+
+      <div className="text-slate-200">
+        {page} / {Math.ceil((2 * Number(per_page)) / Number(per_page))}
+      </div>
+
+      <button
+        className="text-teal-500"
+        disabled={!hasNextPage}
+        onClick={() => {
+          router.push(`/?page=${Number(page) + 1}&per_page=${per_page}`);
+        }}
+      >
+        <BsCaretRightSquareFill size={24} />
+      </button>
+    </div>
+  );
+};
+
+export default PaginationControls;
