ca637ccac52533d6049d1121bb48b5bc58af2900
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-09 22:25:12 +0200

    ui

--- src/components/postView/PostView.tsx
+++ src/components/postView/PostView.tsx
@@ -23,7 +23,7 @@ export const PostView: React.FC<Props> = async ({ post }) => {
   const isLiked = post.likes?.find((like) => like.userId === session?.user.id);
 
   return (
-    <div className="rounded-md bg-indigo-900 hover:bg-indigo-800 duration-300 md:hover:scale-[1.005] shadow-md shadow-slate-950 text-slate-200 w-full">
+    <div className="rounded-md bg-indigo-900 hover:bg-indigo-800 duration-300 hover:shadow-slate-900 shadow-md shadow-slate-800 text-slate-200 w-full">
       <div className="flex items-end">
         <div className="flex justify-start gap-2 px-1 pt-1 md:px-2 md:pt-2">
           <Link href={`/profile/${post.author.name}`} className="">
@@ -52,7 +52,7 @@ export const PostView: React.FC<Props> = async ({ post }) => {
                 <p className="mx-1 font-thin">{` ∙`}</p>
                 <div className="bg-fuchsia-950 rounded-xl px-1 flex py-0.5 gap-1">
                   <span className="flex">
-                    <Icons.Like size={16} className=" text-blue-700 mr-0.5" />
+                    <Icons.Like size={16} className=" text-blue-500 mr-0.5" />
                     {post.likes?.length}
                   </span>
                   |
