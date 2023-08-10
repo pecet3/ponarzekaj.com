c7c7191a0a9fa417d4cd07979613479c2f2b37dd
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-11 00:16:01 +0200

    new parametr

--- src/components/MainTile.tsx
+++ src/components/MainTile.tsx
@@ -2,12 +2,17 @@ import React, { FunctionComponent } from "react";
 
 interface Props {
   children: React.ReactNode;
+  noPadding?: boolean;
 }
 
-export const MainTile: FunctionComponent<Props> = ({ children }) => {
+export const MainTile: FunctionComponent<Props> = ({ children, noPadding }) => {
   return (
     <main className="flex min-h-screen flex-col items-center sm:my-2">
-      <section className="w-full max-w-3xl flex flex-col gap-2 sm:rounded-md rounded-b-md bg-indigo-500 bg-opacity-60 p-1 sm:p-2">
+      <section
+        className={`w-full max-w-3xl flex flex-col gap-2 sm:rounded-md rounded-b-md bg-indigo-500 bg-opacity-60 ${
+          noPadding ? "" : "p-1 sm:p-2"
+        }`}
+      >
         {children}
       </section>
     </main>
