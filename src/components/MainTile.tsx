91da3fdb0fd8f348fd71a57fce436432cbe6e207
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-11 21:02:22 +0200

    ui

--- src/components/MainTile.tsx
+++ src/components/MainTile.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export const MainTile: FunctionComponent<Props> = ({ children, noPadding }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center sm:my-2">
+    <main className="flex min-h-screen flex-col items-center md:my-2">
       <section
-        className={`w-full max-w-3xl flex flex-col gap-2 sm:rounded-md rounded-b-md bg-indigo-500 bg-opacity-60 ${
+        className={`w-full max-w-3xl flex flex-col gap-2 md:rounded-md rounded-b-md bg-indigo-500 bg-opacity-60 ${
           noPadding ? "" : "p-1 sm:p-2"
         }`}
       >
