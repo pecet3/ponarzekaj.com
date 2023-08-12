eb2c872d3f808c976efcbcf3fc5b390bcf4b4260
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-12 20:39:56 +0200

    changed name and props in css

--- src/components/MainTile.tsx
+++ src/components/MainTile.tsx
@@ -2,15 +2,18 @@ import React, { FunctionComponent } from "react";
 
 interface Props {
   children: React.ReactNode;
-  noPadding?: boolean;
+  paddingBottom?: boolean;
 }
 
-export const MainTile: FunctionComponent<Props> = ({ children, noPadding }) => {
+export const MainTile: FunctionComponent<Props> = ({
+  children,
+  paddingBottom,
+}) => {
   return (
     <main className="flex min-h-screen flex-col items-center md:my-2">
       <section
         className={`w-full max-w-3xl flex flex-col gap-2 md:rounded-md rounded-b-md bg-indigo-500 bg-opacity-60 ${
-          noPadding ? "" : "p-1 sm:p-2"
+          paddingBottom ? "pb-1 sm:pb-2" : "p-1 sm:p-2"
         }`}
       >
         {children}
