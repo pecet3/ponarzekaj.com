c8d7161e22e17e6790c13e217d04560b8ef96ae0
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-13 10:05:15 +0200

    ui

--- src/components/MainTile.tsx
+++ src/components/MainTile.tsx
@@ -10,9 +10,9 @@ export const MainTile: FunctionComponent<Props> = ({
   paddingBottom,
 }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center md:my-2">
+    <main className="flex min-h-screen flex-col items-center md:my-2 ">
       <section
-        className={`w-full max-w-3xl flex flex-col gap-2 md:rounded-md rounded-b-md bg-indigo-500 bg-opacity-60 ${
+        className={`w-full max-w-3xl flex flex-col gap-2 md:rounded-md rounded-b-md bg-purple-700 shadow-md shadow-indigo-900 ${
           paddingBottom ? "pb-1 sm:pb-2" : "p-1 sm:p-2"
         }`}
       >
