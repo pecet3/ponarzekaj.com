c839be46c610ebde864ad5b1b270b3ab736b02e5
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-13 17:46:10 +0200

    implemented server action

--- src/app/profile/[name]/page.tsx
+++ src/app/profile/[name]/page.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { PostView } from "@/components/postView/PostView";
 import PaginationControls from "@/components/PaginationControls";
 import { MainTile } from "@/components/MainTile";
+import { Icons } from "@/components/ui/Icons";
+import { getAuthSession } from "@/lib/auth";
 
 interface PageProps {
   params: {
@@ -16,14 +18,19 @@ interface PageProps {
 const page = async ({ params, searchParams }: PageProps) => {
   const { name } = params;
   const decodedName = decodeURI(name);
+
+  const session = await getAuthSession();
   const data = await db.user.findMany({
     where: {
       name: decodedName,
     },
+    include: {
+      friends: true,
+    },
   });
 
   const user = data[0];
-
+  console.log(user);
   const posts = await db.post.findMany({
     where: {
       authorId: user.id ?? "",
@@ -36,6 +43,18 @@ const page = async ({ params, searchParams }: PageProps) => {
   });
 
   if (data.length === 0 || !posts) return <Error />;
+
+  async function addFriend() {
+    "use server";
+    if (!session) return;
+
+    await db.friend.create({
+      data: {
+        userId: user.id,
+        friendId: session?.user.id,
+      },
+    });
+  }
 
   // pagination things
 
@@ -55,7 +74,7 @@ const page = async ({ params, searchParams }: PageProps) => {
           alt={`zdjęcie ${user.name}`}
           height={1280}
           width={720}
-          className="h-64 sm:h-96 sm:rounded-t-md  w-screen"
+          className="h-56 sm:h-96 sm:rounded-t-md  w-screen"
         />
         <Image
           src={user.image ?? ""}
@@ -68,6 +87,11 @@ const page = async ({ params, searchParams }: PageProps) => {
       <span className="flex justify-center flex-col sm:ml-48 ml-40 mt-2 text-slate-200">
         <p className="text-xl sm:text-2xl  font-bold">{user.name}</p>
         <p className="text-base sm:text-xl ">{user.email}</p>
+        <form action={addFriend}>
+          <button type="submit" className="text-white ">
+            <Icons.AddFriend size={16} />
+          </button>
+        </form>
       </span>
       <div className="mt-10 p-1 sm:p-2 flex flex-col gap-2 ">
         {entries.map((post) => {
