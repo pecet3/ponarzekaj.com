a40803e659d7d1b507a99641d44ec8517ba3e608
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-14 17:40:54 +0200

    import paths

--- src/app/profile/friends/page.tsx
+++ src/app/profile/friends/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { db } from "@/lib/db";
 import Error from "@/components/Error";
 import Image from "next/image";
-import { PostView } from "@/components/postView/PostView";
+import { PostView } from "@/components/post/PostView";
 import PaginationControls from "@/components/PaginationControls";
 import { MainTile } from "@/components/MainTile";
 import { Icons } from "@/components/ui/Icons";
 import { getAuthSession } from "@/lib/auth";
 import { isUserThing } from "@/lib/helpers";
+import { FriendRequest } from "@/components/FriendRequest";
+import { Main } from "@/components/Main";
 
 interface PageProps {
   params: {
@@ -25,31 +27,14 @@ const page = async () => {
       accepted: false,
     },
   });
-
-  async function acceptRequest() {
-    "use server";
-  }
-
   return (
-    <MainTile>
-      {friends.map(async (friend) => {
-        const userInfo = await db.user.findUnique({
-          where: {
-            id: friend.friendId,
-          },
-        });
-        return (
-          <div key={friend.id} className="flex">
-            {userInfo?.name}
-            <form action={acceptRequest}>
-              <button type="submit" formAction={acceptRequest}>
-                accept
-              </button>
-            </form>
-          </div>
-        );
-      })}
-    </MainTile>
+    <Main>
+      <section className="flex max-w-3xl w-full flex-col gap-2">
+        {friends.map((friend) => (
+          <FriendRequest key={friend.id} friend={friend} />
+        ))}
+      </section>
+    </Main>
   );
 };
 
