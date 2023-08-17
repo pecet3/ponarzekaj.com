5ce0bd956b830da9d230d36771ecd0dc85306c4a
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-17 17:11:18 +0100

    searchParams

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,7 +4,10 @@ import * as React from "react";
 import { FC } from "react";
 import { Icons } from "../../../components/ui/Icons";
 
-const UserAuthForm = () => {
+const UserAuthForm: React.FunctionComponent<{ searchParams: string }> = ({
+  searchParams,
+}) => {
+  const firstTime = searchParams;
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   const loginWithGoogle = async () => {
@@ -20,22 +23,45 @@ const UserAuthForm = () => {
 
   return (
     <main className="flex flex-col background min-h-screen">
-      <div className="flex flex-col  mt-6 mx-auto bg-slate-400 p-2 rounded-md">
-        <h1 className="font-bold mb-2 text-xl">Zaloguj się za pomocą:</h1>
-        <button
-          type="button"
-          className="w-full btn btn-primary"
-          onClick={loginWithGoogle}
-          disabled={isLoading}
-        >
-          {isLoading ? (
-            <Icons.Spinner size={24} className="animate-spin" />
-          ) : (
-            <Icons.Google size={24} />
-          )}
-          Google
-        </button>
-      </div>
+      {!firstTime ? (
+        <div className="flex flex-col  mt-6 mx-auto bg-slate-400 p-2 rounded-md">
+          <h1 className="font-bold mb-2 text-xl">Zaloguj się za pomocą:</h1>
+          <button
+            type="button"
+            className="rounded-md p-1 text-xl bg-blue-700 text-slate-300 font-semibold hover:bg-sky-700 duration-300 flex justify-center"
+            onClick={loginWithGoogle}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <Icons.Spinner size={24} className="animate-spin" />
+            ) : (
+              <Icons.Google size={24} />
+            )}
+            Google
+          </button>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center mt-6 mx-auto bg-slate-400 p-2 rounded-md">
+          <h1 className="font-bold mb-2 text-xl">Zarejestruj się za pomocą:</h1>
+          <p className="text-sm">
+            {" "}
+            na ten moment to jest jedyna opcja rejestracji
+          </p>
+          <button
+            type="button"
+            className="rounded-md p-1 text-xl bg-blue-700 text-slate-300 font-semibold hover:bg-sky-700 duration-300 flex justify-center"
+            onClick={loginWithGoogle}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <Icons.Spinner size={24} className="animate-spin" />
+            ) : (
+              <Icons.Google size={24} />
+            )}
+            Google
+          </button>
+        </div>
+      )}
     </main>
   );
 };
