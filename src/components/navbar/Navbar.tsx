2054050a5ee8d227c11c9a339ec45ce78be6be50
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-17 17:12:21 +0100

    .

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Icons } from "../ui/Icons";
 import { ProfileList } from "./ProfileList";
 import { db } from "@/lib/db";
+import { NotificationView } from "../NotificationView";
 
 export const Navbar = async () => {
   const session = await getAuthSession();
@@ -12,7 +13,12 @@ export const Navbar = async () => {
     where: {
       id: session ? session?.user.id : "",
     },
+    include: {
+      notifications: true,
+    },
   });
+
+  console.log(user?.notifications);
   return (
     <nav className="navbar bg-slate-700 text-slate-200">
       <div className="flex-1">
@@ -28,28 +34,41 @@ export const Navbar = async () => {
       </div> */}
       <div className="flex-none gap-2">
         {session ? (
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <Image
-                  src={session.user.image ?? ""}
-                  height={96}
-                  width={96}
-                  alt="Your avatar"
+          <>
+            <div className="dropdown dropdown-end bg-slate-700">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <Image
+                    src={session.user.image ?? ""}
+                    height={96}
+                    width={96}
+                    alt="Your avatar"
+                  />
+                </div>
+              </label>
+              <ProfileList username={user?.name ?? "Ja kub"} />
+            </div>
+            <div className="dropdown dropdown-end bg-slate-700">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <Icons.Comments size={24} />
+                </div>
+              </label>
+              {user?.notifications.map((notification) => (
+                <NotificationView
+                  key={notification.id}
+                  notification={notification}
                 />
-              </div>
-            </label>
-            <ProfileList username={user?.name ?? "Ja kub"} />
-          </div>
+              ))}
+            </div>
+          </>
         ) : (
-          <div className="flex-1">
-            <Link
-              className="btn btn-primary normal-case text-xl"
-              href="/sign-in"
-            >
-              Zaloguj się
-            </Link>
-          </div>
+          <Link
+            className="rounded-md p-1 text-xl bg-blue-600 text-slate-300 font-semibold hover:bg-sky-600 duration-300"
+            href="/sign-in"
+          >
+            Zaloguj się
+          </Link>
         )}
       </div>
     </nav>
