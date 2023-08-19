1ec95e40008e9c7662ce0e7806ab7b9eeb948680
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-19 22:05:10 +0100

    .

--- src/components/NotificationView.tsx
+++ src/components/NotificationView.tsx
@@ -3,6 +3,7 @@ import { Notification } from "@prisma/client";
 import Link from "next/link";
 import { db } from "@/lib/db";
 import Image from "next/image";
+
 export const NotificationView: FunctionComponent<{
   notification: Notification;
 }> = async ({ notification }) => {
@@ -11,11 +12,25 @@ export const NotificationView: FunctionComponent<{
       id: notification.authorId,
     },
   });
+  await db.notification.update({
+    where: {
+      id: notification.id,
+    },
+    data: {
+      visited: true,
+    },
+  });
   if (!author) return null;
   return (
-    <li className="rounded-lg bg-slate-700 hover:bg-slate-800 duration-300 m-auto">
+    <li
+      className={`rounded-lg ${
+        notification.visited
+          ? "bg-slate-600 hover:bg-slate-700"
+          : "bg-slate-700 hover:bg-slate-800"
+      } duration-300 m-auto`}
+    >
       <Link
-        href={notification.link ?? "/"}
+        href={`${notification.link}?notificationId=${notification?.id}` ?? "/"}
         className="flex justify-between gap-2 items-center"
       >
         <Image
