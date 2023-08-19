7d954cdc312eea37c5549504eeea96214ff9a34a
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-20 00:57:58 +0100

    working notifications

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -20,10 +20,29 @@ export const Navbar = async () => {
     },
   });
 
-  const notifications = user?.notifications;
+  const visitedNotifications = await db.notification.findMany({
+    where: {
+      userId: session?.user.id,
+      visited: false,
+    },
+  });
+
+  const visitedNotificationsLength = visitedNotifications.length;
+
+  async function markVisitedNotificationAsTrue() {
+    "use server";
+    await db.notification.updateMany({
+      where: {
+        userId: session?.user.id,
+      },
+      data: {
+        visited: true,
+      },
+    });
+  }
 
   return (
-    <nav className="navbar bg-slate-700 text-slate-200">
+    <nav className="navbar bg-slate-700 text-slate-200 p-0 sm:p-1">
       <div className="flex-1">
         <Link className="btn btn-ghost normal-case text-xl p-1 sm:p-2" href="/">
           ponarzekaj.com
@@ -38,24 +57,37 @@ export const Navbar = async () => {
       <div className="flex-none gap-2">
         {session ? (
           <>
-            <div className="dropdown dropdown-end bg-slate-700">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="m-auto relative">
-                  <Icons.Notification size={32} />
+            <form action={markVisitedNotificationAsTrue}>
+              <button type="submit">
+                <div className="dropdown dropdown-end bg-slate-700">
+                  <label
+                    tabIndex={0}
+                    className="relative btn btn-ghost btn-circle avatar"
+                  >
+                    <div className="m-auto">
+                      <Icons.Notification size={32} />
+                    </div>
+                    {true ? (
+                      <p className="absolute z-10 top-2 right-2 rounded-full px-0.5 text-slate-100 bg-red-700">
+                        {visitedNotificationsLength}
+                      </p>
+                    ) : null}
+                  </label>
+                  <ul
+                    tabIndex={0}
+                    className="mt-3 z-[1] p-2 shadow menu-sm dropdown-content bg-base-100 rounded-box w-64 flex-col flex gap-1"
+                  >
+                    {user?.notifications.map((notification) => (
+                      <NotificationView
+                        key={notification.id}
+                        notification={notification}
+                      />
+                    ))}
+                  </ul>
                 </div>
-              </label>
-              <ul
-                tabIndex={0}
-                className="mt-3 z-[1] p-2 shadow menu-sm dropdown-content bg-base-100 rounded-box w-64 flex-col flex gap-1"
-              >
-                {user?.notifications.map((notification) => (
-                  <NotificationView
-                    key={notification.id}
-                    notification={notification}
-                  />
-                ))}
-              </ul>
-            </div>
+              </button>
+            </form>
+
             <div className="dropdown dropdown-end bg-slate-700">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
@@ -72,7 +104,7 @@ export const Navbar = async () => {
             </div>
           </>
         ) : (
-          <Link className="majorButton" href="/sign-in">
+          <Link className="majorButton mr-2" href="/sign-in">
             Zaloguj się
           </Link>
         )}
