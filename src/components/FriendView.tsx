4576f462d9faf6b0e1882b906f1e44a404c2089e
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-23 22:06:36 +0200

    ui

---
+++ src/components/FriendView.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import type { Friend } from "@prisma/client";
+import { db } from "@/lib/db";
+import Image from "next/image";
+import { Icons } from "./ui/Icons";
+import { revalidatePath } from "next/cache";
+import { getAuthSession } from "@/lib/auth";
+import Link from "next/link";
+interface IProvidersProps {
+  friend: Friend;
+}
+
+export const FriendView: React.FunctionComponent<IProvidersProps> = async ({
+  friend,
+}) => {
+  const session = await getAuthSession();
+  if (!session) return null;
+  const user = await db.user.findUnique({
+    where: {
+      id: friend.friendId,
+    },
+  });
+
+  if (!user) return null;
+
+  const encodedUsername = encodeURIComponent(user.name as string);
+  return (
+    <li className="">
+      <Link
+        href={`/profile/${encodedUsername}`}
+        className="bg-blue-900 text-slate-200 rounded-lg
+         shadow-md shadow-gray-800 w-full p-2 flex gap-2"
+      >
+        <Image
+          src={user.image ?? ""}
+          width={96}
+          height={96}
+          className="rounded-full h-14 w-14"
+          alt="Zdjęcie użytkownika co chce dodać Cię do znajomych"
+        />
+        <span>
+          <p className="text-lg sm:text-xl font-semibold">{user.name}</p>
+          <p className="text-base sm:text-sm text-slate-400">{user.email}</p>
+        </span>
+      </Link>
+    </li>
+  );
+};
