5f6b5ae8d86723dd69382fa336475c2673e33fd5
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-24 22:26:28 +0200

    .

--- src/components/MainTile.tsx
+++ src/components/MainTile.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const MainTile: FunctionComponent<Props> = ({ children }) => {
   return (
     <Main>
-      <section className="w-full max-w-3xl flex flex-col md:rounded-xl bg-transparent sm:bg-slate-800 sm:shadow-md shadow-black ">
+      <section className="w-full max-w-3xl flex flex-col md:rounded-xl bg-transparent sm:bg-slate-800 shadow-none sm:shadow-md sm:shadow-gray-700">
         {children}
       </section>
     </Main>
