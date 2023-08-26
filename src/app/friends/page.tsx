558655b5d73f24b587a6b81a9e7191abedb7a745
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-26 12:42:43 +0200

    ui

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -1,15 +1,9 @@
 import React, { FunctionComponent } from "react";
 import { db } from "@/lib/db";
-import Error from "@/components/Error";
-import Image from "next/image";
-import { PostView } from "@/components/post/PostView";
-import PaginationControls from "@/components/PaginationControls";
+
 import { MainTile } from "@/components/MainTile";
-import { Icons } from "@/components/ui/Icons";
 import { getAuthSession } from "@/lib/auth";
-import { isUserThing } from "@/lib/helpers";
 import { FriendRequest } from "@/components/FriendRequest";
-import { Main } from "@/components/Main";
 import { FriendView } from "@/components/FriendView";
 import { SearchUser } from "@/components/SearchAUser";
 interface PageProps {
@@ -29,7 +23,7 @@ const page: FunctionComponent<PageProps> = async ({ searchParams }) => {
 
   return (
     <MainTile>
-      <div className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 p-3 h-64 rounded-t-lg border-b-2 border-slate-400 overflow-y-scroll">
+      <div className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 p-3 h-32 rounded-t-lg border-b-2 border-slate-400 overflow-y-scroll">
         <p>Zaproszenia do znajomych</p>
         {friends.map((friend) => {
           if (friend.accepted === true || friend.initiator === true) return;
@@ -42,6 +36,9 @@ const page: FunctionComponent<PageProps> = async ({ searchParams }) => {
           );
         })}
       </div>
+      <div className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 p-3 h-32 rounded-t-lg border-b-2 border-slate-400 overflow-y-scroll">
+        {/* <SearchUser /> */}
+      </div>
       <div className="flex max-w-3xl w-full flex-col bg-slate-800 gap-2 p-3 h-64 rounded-b-lg overflow-y-scroll">
         <p>Lista znajomych</p>
         {friends.map((friend) => {
@@ -49,7 +46,6 @@ const page: FunctionComponent<PageProps> = async ({ searchParams }) => {
           return <FriendView key={friend.id} friend={friend} />;
         })}
       </div>
-      {/* <SearchUser /> */}
     </MainTile>
   );
 };
