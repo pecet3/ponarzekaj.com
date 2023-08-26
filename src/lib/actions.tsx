79fd509de6d19683a382e4fbcfdaf4805d038647
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-26 12:43:18 +0200

    .

---
+++ src/lib/actions.tsx
@@ -0,0 +1,12 @@
+// export const searchUserByName = async () => {
+//     {
+//       try {
+//         const users = await db.user.findMany({
+//           where: {
+//             name: input,
+//           },
+//         });
+//         setResults(users);
+//       } catch (error) {}
+//     }
+//   };
