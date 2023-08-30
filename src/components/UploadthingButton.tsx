bbe46ff4874240105bbb6654598dd1b03aca5222
Jakub Pacewicz <pecet3107@gmail.com>
2023-08-31 00:01:10 +0200

    ...

--- src/components/UploadthingButton.tsx
+++ src/components/UploadthingButton.tsx
@@ -1,57 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import axios from "axios";
 import { UploadButton } from "@/utils/uploadthing";
-
+import "@uploadthing/react/styles.css";
+import { Icons } from "./ui/Icons";
 interface UploadthingButtonProps {
-  onImageUpload: (fileUrl: string) => void;
+  setImageUrl: (fileUrl: string) => void;
 }
 
 const UploadthingButton: React.FC<UploadthingButtonProps> = ({
-  onImageUpload,
+  setImageUrl,
 }) => {
-  const handleImageChange = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const selectedFile = event.target.files && event.target.files[0];
-
-    if (selectedFile) {
-      try {
-        const formData = new FormData();
-        formData.append("file", selectedFile);
-
-        const response = await axios.post("/api/uploadthing", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        });
-
-        if (response.data && onImageUpload) {
-          onImageUpload(response.data.fileUrl);
-        }
-      } catch (error) {
-        console.error("Error uploading image:", error);
-      }
-    }
-  };
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <div className="">
-      <UploadButton
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          if (typeof res === "undefined") return;
-          const url = res[0]?.fileUrl as string;
-        }}
-        onUploadError={(error: Error) => {
-          alert(`ERROR! ${error.message}`);
-        }}
-        appearance={{
-          button:
-            "ut-ready:bg-green-500 ut-uploading:cursor-not-allowed rounded-r-none bg-red-500 bg-none after:bg-orange-400 text-transparent h-4",
-          container: "w-8 flex-row rounded-md border-cyan-300 bg-slate-800 m-0",
-          allowedContent: "hidden",
-        }}
-      />
+    <div className="relative hover:cursor">
+      <Icons.Image className="absolute top-0 right-0" size={20} />
+      <div className="opacity-50">
+        <UploadButton
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            if (typeof res === "undefined") return;
+            const url = res[0]?.url as string;
+            setImageUrl(url);
+          }}
+          onUploadError={(error: Error) => {
+            alert(`ERROR! ${error.message}`);
+          }}
+          className=""
+        />
+      </div>
     </div>
   );
 };
