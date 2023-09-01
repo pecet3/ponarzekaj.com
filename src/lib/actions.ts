5b1a01b407a395e1c463fc2f1987f30fa7e9dd3a
Jakub Pacewicz <pecet3107@gmail.com>
2023-09-01 20:53:18 +0200

    .

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -5,54 +5,59 @@ import { db } from "./db";
 import { ratelimit } from "./redis";
 import { getAuthSession } from "./auth";
 
-// const createAPost = async (formData: FormData) => {
-//   try {
-//     const { success } = await ratelimit.post.limit(authorId);
+const createAPost = async (
+  form: FormData,
+  authorId: string,
+  emoji: string,
+  content: string
+) => {
+  try {
+    const { success } = await ratelimit.post.limit(authorId);
 
-//     if (!success) {
-//       return new Response("TOO MANY REQUESTS", { status: 403 });
-//     }
+    if (!success) {
+      return new Response("TOO MANY REQUESTS", { status: 403 });
+    }
 
-//     const post = await db.post.create({
-//       data: {
-//         authorId,
-//         emoji,
-//         content,
-//       },
-//     });
+    const post = await db.post.create({
+      data: {
+        authorId,
+        emoji,
+        content,
+      },
+    });
 
-//     const user = await db.user.findUnique({
-//       where: {
-//         id: authorId,
-//       },
-//       select: {
-//         friends: {
-//           where: {
-//             accepted: true,
-//           },
-//         },
-//       },
-//     });
-//     if (!user) return;
+    const user = await db.user.findUnique({
+      where: {
+        id: authorId,
+      },
+      select: {
+        friends: {
+          where: {
+            accepted: true,
+          },
+        },
+      },
+    });
+    if (!user) return;
 
-//     for (const friend of user.friends) {
-//       await db.notification.create({
-//         data: {
-//           userId: friend.friendId,
-//           content: "opublikował post",
-//           link: `/post/${post.id}`,
-//           authorId: authorId,
-//         },
-//       });
-//     }
+    for (const friend of user.friends) {
+      await db.notification.create({
+        data: {
+          userId: friend.friendId,
+          content: "opublikował post",
+          link: `/post/${post.id}`,
+          authorId: authorId,
+        },
+      });
+    }
 
-//     revalidatePath("/");
-//   } catch (error) {
-//     return {
-//       error,
-//     };
-//   }
-// };
+    revalidatePath("/");
+  } catch (error) {
+    return {
+      error,
+    };
+  }
+};
 export const createAComment = async (form: FormData, postId: string) => {
   const session = await getAuthSession();
   const content = form.get("content");
