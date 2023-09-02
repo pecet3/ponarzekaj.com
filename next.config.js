818794b868e0addc266caf9da525457c98e10a63
Jakub Pacewicz <pecet3107@gmail.com>
2023-09-02 20:15:32 +0200

    new path for images

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
                 port: '',
                 pathname: '**',
             },
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                port: '',
+                pathname: '**',
+            },
         ],
     },
     experimental: {
