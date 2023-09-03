ce768b07967fd2ff8970fead7efd7fad2bdcd2e0
Jakub Pacewicz <pecet3107@gmail.com>
2023-09-04 00:51:14 +0200

    new component

---
+++ src/components/SubmitButton.tsx
@@ -0,0 +1,19 @@
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { Icons } from "./ui/Icons";
+
+export const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="submit"
+      className="m-0 text-slate-50 rounded-md bg-blue-700 py-1 px-1.5 text-sm transition-all duration-300 hover:bg-blue-600 md:text-base w-full text-center"
+      disabled={pending}
+    >
+      {pending ? (
+        <Icons.Spinner className="animate-spin m-auto" size={20} />
+      ) : (
+        "Dodaj"
+      )}
+    </button>
+  );
+};
