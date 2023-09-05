eb0c64c983e8ea3aa1efa48d37c7c50be28e664e
Jakub Pacewicz <pecet3107@gmail.com>
2023-09-05 21:26:24 +0200

    ui

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -23,7 +23,7 @@ const page: FunctionComponent<PageProps> = async ({ searchParams }) => {
 
   return (
     <MainTile>
-      <div className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 p-3 h-32 rounded-t-lg border-b-2 border-slate-400 overflow-y-scroll">
+      <ul className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 p-3 h-32 rounded-t-lg border-b-2 border-slate-400 overflow-y-scroll">
         <p>Zaproszenia do znajomych</p>
         {friends.map((friend) => {
           if (friend.accepted === true || friend.initiator === true) return;
@@ -35,10 +35,12 @@ const page: FunctionComponent<PageProps> = async ({ searchParams }) => {
             />
           );
         })}
-      </div>
+      </ul>
+
       <div className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 p-3 h-32 rounded-t-lg border-b-2 border-slate-400 overflow-y-scroll">
         <SearchUser />
       </div>
+
       <ul className="flex max-w-3xl w-full flex-col bg-slate-800 gap-2 p-3 h-64 rounded-b-lg overflow-y-scroll">
         <p>Lista znajomych</p>
         {friends.map((friend) => {
