1e13eaa0d9fa881688aaf46b8ca9af9d3002a1ca
Jakub Pacewicz <pecet3107@gmail.com>
2023-09-05 23:27:27 +0200

    ui asnd logic

--- src/components/SearchAUser.tsx
+++ src/components/SearchAUser.tsx
@@ -24,37 +24,60 @@ export const SearchUser: FC<PageProps> = ({}) => {
   };
 
   return (
-    <section>
-      <div className="flex">
+    <>
+      <div className="flex gap-1">
         <input
           className="rounded-lg p-1"
           value={input}
           type="text"
           placeholder="Wyszukaj użytkownika 🔍"
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              if (input !== "") {
+                searchHandle();
+              }
+            }
+          }}
         />
-        <button onClick={searchHandle}>szukaj</button>
+        {/* <button
+          onClick={searchHandle}
+          className="rounded-lg bg-slate-300 text-black px-1 hover:bg-slate-100 duration-300 hover:rounded-md"
+        >
+          Szukaj 🔍
+        </button> */}
       </div>
-      <div>
+      <ul className="flex flex-col gap-2  overflow-y-scroll overflow-hidden">
         {results
           ? results.map((user) => (
-              <Link
-                key={user.id}
-                href={`/profile/${user.name}`}
-                className="flex gap-2"
-              >
-                <Image
-                  className="rounded-full h-12 w-12"
-                  width={48}
-                  height={48}
-                  alt="zdjęcie użytkownika"
-                  src={user.image ?? ""}
-                />
-                {user.name}
-              </Link>
+              <li className="list-none flex justify-center items-center mr-3">
+                <Link
+                  href={`/profile/${user.name}`}
+                  className="bg-blue-900 text-slate-200 rounded-lg
+               shadow-md shadow-gray-800 w-full px-1 sm:p-2 flex gap-2 items-center"
+                >
+                  <Image
+                    src={user.image ?? ""}
+                    width={96}
+                    height={96}
+                    className="rounded-full h-8 w-8 sm:h-12 sm:w-12"
+                    alt="Zdjęcie użytkownika"
+                  />
+
+                  <span>
+                    <p className="text-lg sm:text-xl font-semibold m-0">
+                      {user.name}
+                    </p>
+                    <p className="text-base sm:text-sm text-slate-400 m-0">
+                      {user.email}
+                    </p>
+                  </span>
+                </Link>
+              </li>
             ))
           : null}
-      </div>
-    </section>
+      </ul>
+    </>
   );
 };
