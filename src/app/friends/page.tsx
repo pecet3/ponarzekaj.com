eaa74e712f091b978ca67094036a7dd7e3b9ed41
Jakub Pacewicz <pecet3107@gmail.com>
2023-09-05 23:27:16 +0200

    ui

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -23,31 +23,35 @@ const page: FunctionComponent<PageProps> = async ({ searchParams }) => {
 
   return (
     <MainTile>
-      <ul className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 p-3 h-32 rounded-t-lg border-b-2 border-slate-400 overflow-y-scroll">
-        <p>Zaproszenia do znajomych</p>
-        {friends.map((friend) => {
-          if (friend.accepted === true || friend.initiator === true) return;
-          return (
-            <FriendRequest
-              key={friend.id}
-              friend={friend}
-              notificationId={notificationId as string}
-            />
-          );
-        })}
-      </ul>
+      <section>
+        <p className="ml-3 mt-1">Zaproszenia do znajomych</p>
+        <ul className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 p-3 h-32 rounded-t-lg border-b-2 border-slate-400 overflow-y-scroll">
+          {friends.map((friend) => {
+            if (friend.accepted === true || friend.initiator === true) return;
+            return (
+              <FriendRequest
+                key={friend.id}
+                friend={friend}
+                notificationId={notificationId as string}
+              />
+            );
+          })}
+        </ul>
+      </section>
 
-      <div className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 p-3 h-32 rounded-t-lg border-b-2 border-slate-400 overflow-y-scroll">
+      <section className="flex max-w-3xl w-full flex-col gap-2 bg-slate-800 pl-3 py-3 h-32 rounded-t-lg border-b-2 border-slate-400">
         <SearchUser />
-      </div>
-
-      <ul className="flex max-w-3xl w-full flex-col bg-slate-800 gap-2 p-3 h-64 rounded-b-lg overflow-y-scroll">
-        <p>Lista znajomych</p>
-        {friends.map((friend) => {
-          if (friend.accepted === false) return;
-          return <FriendView key={friend.id} friend={friend} />;
-        })}
-      </ul>
+      </section>
+      <section>
+        <p className="ml-3 mt-1">Lista znajomych</p>
+        <ul className="flex max-w-3xl w-full flex-col bg-slate-800 gap-2 p-3 h-64 rounded-b-lg overflow-y-scroll">
+          {friends.map((friend) => {
+            if (friend.accepted === false) return;
+            return <FriendView key={friend.id} friend={friend} />;
+          })}
+        </ul>
+      </section>
+      <div className="opacity-0">.</div>
     </MainTile>
   );
 };
