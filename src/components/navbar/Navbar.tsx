b2abadba3b3f36fb681fd82ece80369f544316ad
Jakub Pacewicz <pecet3107@gmail.com>
2023-09-06 00:05:12 +0200

    changed link to login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -123,7 +123,7 @@ export const Navbar = async () => {
         ) : (
           <Link
             className="rounded-lg p-1 text-xl bg-blue-700 text-slate-100 font-semibold hover:bg-blue-600 duration-300 ring-1 ring-blue-600 mr-2"
-            href="/sign-up"
+            href="/sign-in"
           >
             Zaloguj się
           </Link>
