fd2b22d56c5fd1040d540caff1dd46f78216ab49
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-13 20:14:09 +0200

    fixed footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,20 +23,28 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
-          <footer className="bg-slate-700 text-slate-300 text-sm flex gap-2 items-center p-2 justify-center mt-8 ">
-            <p className="">Strona powstała w celu nauki oraz zabawy.</p>
-            <p>
-              <a
-                href="https://github.com/pecet3"
-                className="text-slate-50 font-bold"
-              >
-                @Jakub Pacewicz
-              </a>
-              , 2023
-            </p>
-            <a href="https://discord.gg/jDxBNk6M" className="text-center">
-              <Icons.Discord size={32} className="text-slate-20" />
-              Discord społeczności
+          <footer className="bg-slate-700 text-slate-300 text-sm flex gap-4 items-center p-2 justify-center mt-8 ">
+            <div>
+              <p className="">Strona powstała w celu nauki oraz zabawy.</p>
+              <p>
+                <a
+                  href="https://github.com/pecet3"
+                  className="text-slate-50 font-bold"
+                >
+                  @Jakub Pacewicz
+                </a>
+                , 2023
+              </p>
+            </div>
+            <a
+              href="https://discord.gg/jDxBNk6M"
+              className="text-center text-xs"
+            >
+              <Icons.Discord
+                size={32}
+                className="text-slate-50 hover:text-slate-300 duration-300 m-auto"
+              />
+              Nasz Discord
             </a>
           </footer>
         </Providers>
