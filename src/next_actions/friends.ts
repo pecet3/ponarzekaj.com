33ffb72f1b4ee53b390af44a50f01909f4d7c7b8
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-13 20:14:25 +0200

    error handling

--- src/next_actions/friends.ts
+++ src/next_actions/friends.ts
@@ -3,77 +3,77 @@ import { revalidatePath } from 'next/cache';
 import { db } from "@/lib/db";
 import { getAuthSession } from '../lib/auth';
 import { UserWithFriends } from '../types/prisma';
-
+import { Friend } from "@prisma/client"
 export async function addFriend(user: UserWithFriends) {
+  try {
+    const session = await getAuthSession()
+    if (!session) throw new Error("Unauthorized user");
 
-  const session = await getAuthSession()
-  if (!session)
-    return
-
-  await db.friend.create({
-    data: {
-      userId: user.id ?? "",
-      friendId: session?.user.id ?? "",
-    },
-  });
-  await db.friend.create({
-    data: {
-      userId: session?.user.id ?? "",
-      friendId: user.id ?? "",
-      initiator: true,
-    },
-  });
-  await db.notification.create({
-    data: {
-      userId: user.id ?? "",
-      authorId: session.user.id ?? "",
-      content: "Wysłał Ci zaproszenie do znajomych",
-      link: `/friends`,
-    },
-  });
-  revalidatePath("/");
-};
-
-const acceptFriend = async () => {
-  "use server";
-
-  await db.friend.updateMany({
-    where: {
-      friendId: session?.user.id,
-      userId: friend.friendId,
-    },
-    data: {
-      accepted: true,
-    },
-  });
-
-  await db.friend.update({
-    where: {
-      id: friend.id,
-    },
-    data: {
-      accepted: true,
-    },
-  });
-  const friendData = await db.user.findUnique({
-    where: {
-      id: friend.friendId,
-    },
-  });
-  await db.notification.create({
-    data: {
-      userId: friend.friendId,
-      content: `zaakceptował Twoje zaproszenie`,
-      link: `/profile/${friendData?.name}`,
-      authorId: session?.user.id as string,
-    },
-  });
-  if (!!notificationId) {
-    await db.notification.delete({
-      where: {
-        id: notificationId,
+    await db.friend.create({
+      data: {
+        userId: user.id ?? "",
+        friendId: session?.user.id ?? "",
       },
     });
+    await db.friend.create({
+      data: {
+        userId: session?.user.id ?? "",
+        friendId: user.id ?? "",
+        initiator: true,
+      },
+    });
+    await db.notification.create({
+      data: {
+        userId: user.id ?? "",
+        authorId: session.user.id ?? "",
+        content: "Wysłał Ci zaproszenie do znajomych",
+        link: `/friends`,
+      },
+    });
+    revalidatePath("/");
+  } catch (error) {
+    return { error }
   }
-  revalidatePath("/profile");
+};
+
+export async function acceptFriend(friend: Friend) {
+  try {
+    const session = await getAuthSession()
+
+    await db.friend.updateMany({
+      where: {
+        friendId: session?.user.id,
+        userId: friend.friendId,
+      },
+      data: {
+        accepted: true,
+      },
+    });
+
+    await db.friend.update({
+      where: {
+        id: friend.id,
+      },
+      data: {
+        accepted: true,
+      },
+    });
+    const friendData = await db.user.findUnique({
+      where: {
+        id: friend.friendId,
+      },
+    });
+    await db.notification.create({
+      data: {
+        userId: friend.friendId,
+        content: `zaakceptował Twoje zaproszenie`,
+        link: `/profile/${friendData?.name}`,
+        authorId: session?.user.id as string,
+      },
+    });
+
+    revalidatePath("/profile");
+  } catch (error) {
+    return { error }
+  }
 };
