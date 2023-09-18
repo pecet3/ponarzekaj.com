c2b33da282a42506c3aa9cd3a764ed607f5de4d0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-18 19:20:45 +0200

    comment validator

--- src/next_actions/comment.ts
+++ src/next_actions/comment.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { db } from "@/lib/db";
 import { ratelimit } from "@/lib/redis";
 import { getAuthSession } from "@/lib/auth";
+import { commentValidator } from '../lib/validators';
 
 export const createAComment = async (form: FormData, postId: string) => {
   try {
@@ -51,3 +52,54 @@ export const createAComment = async (form: FormData, postId: string) => {
     revalidatePath(`/post/${postId}`);
   }
 };
+
+export const addALike = async (userId: string, commentId: string) => {
+  try {
+
+    const body = commentValidator.parse({ userId, commentId });
+
+    const session = await getAuthSession();
+
+    if (!session) {
+      throw new Error("Unauthorized");
+    }
+    const { success } = await ratelimit.notification.limit(session.user.id);
+
+    if (!success) {
+      throw new Error("Too many requests");
+    }
+
+    if (session.user.id !== userId) {
+      throw new Error("Unauthorized");
+    }
+    const comment = await db.comment.findUnique({
+      where: {
+        id: body.commentId,
+      },
+    });
+    if (!comment) {
+      throw new Error("Server error");
+    }
+    if (session.user.id !== comment.authorId) {
+      await db.notification.create({
+        data: {
+          userId: comment.authorId,
+          authorId: userId,
+          content: "polubił Ci komentarz!",
+          link: `/post/${comment.postId}`,
+        },
+      });
+    }
+
+    await db.likeComment.create({
+      data: {
+        userId: body.userId,
+        commentId: body.commentId,
+      },
+    });
+
+    return new Response("OK", { status: 200 });
+  } catch (error) {
+    return { error }
+  }
+}
