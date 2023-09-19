afc0dd4233a4a0cab332a2b1d814f6ae5c041de3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-19 22:02:43 +0200

    .

---
+++ src/components/auth/RegisterForm.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { SubmitButton } from "../SubmitButton";
+import { toast } from "react-hot-toast";
+import { register } from "../../next_actions/auth";
+import { useRouter } from "next/navigation";
+
+export const RegisterForm = () => {
+  const router = useRouter();
+  return (
+    <form
+      className="flex flex-col gap-1 justify-center items-center text-slate-200"
+      action={async (form) => {
+        const password = form.getAll("password");
+        const repeatedPassword = form.getAll("repetedPassword");
+
+        if (password !== repeatedPassword)
+          return toast.error("Hasła nie są takie same!");
+        await register(form);
+        router.push("/sign-in");
+      }}
+    >
+      <input
+        type="email"
+        className="p-1  rounded-md text-sm"
+        name="email"
+        placeholder="email"
+        required
+      />
+      <input
+        type="text"
+        className="p-1  rounded-md text-sm"
+        name="name"
+        placeholder="imię i nazwisko"
+        required
+      />
+      <input
+        type="password"
+        className="p-1 rounded-md text-sm"
+        name="password"
+        placeholder="hasło"
+        required
+      />
+      <input
+        type="password"
+        className="p-1 rounded-md text-sm"
+        name="repeatedPassword"
+        placeholder="powtórz hasło"
+        required
+      />
+      <SubmitButton />
+    </form>
+  );
+};
