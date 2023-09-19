8d6b8b63a85b027c384787c7f301265af55d99ca
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-19 22:02:08 +0200

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const { withUt } = require("uploadthing/tw");
- 
+
 module.exports = withUt({
   darkMode: 'class',
   content: [
@@ -17,7 +17,7 @@ module.exports = withUt({
       },
     },
   },
-  
+
   screens: {
     'xs': '480px',
     'sm': '640px',
@@ -38,6 +38,10 @@ module.exports = withUt({
 
   daisyui: {
     themes: ["light", "dark", "cupcake"],
+  },
+  fontFamily: {
+    'nunito': ['nunito', 'sans-serif'],
+    'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
   },
   plugins: [require("daisyui")],
 });
