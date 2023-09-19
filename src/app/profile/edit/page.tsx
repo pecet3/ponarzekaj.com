16537e7d8302a534c13c39c66fbeadf0da5d3e84
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-19 22:02:17 +0200

    .

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -6,25 +6,16 @@ import { MainTile } from "@/components/MainTile";
 import { Icons } from "@/components/ui/Icons";
 import { Error } from "@/components/Error";
 
-
-
-
-const page= async () => {
-  
-
-
+const page = async () => {
   const session = await getAuthSession();
- 
+
   const user = await db.user.findUnique({
     where: {
-      id:session?.user.id
+      id: session?.user.id,
     },
   });
 
   if (!user || !session) return <Error />;
- 
-
-
 
   // const handleImageChange = (event) => {
   //   setBackgroundImage(URL.createObjectURL(event.target.files[0]));
@@ -34,20 +25,22 @@ const page= async () => {
     <MainTile>
       <div className="relative">
         <Image
-          src={user.backgroundImage}
+          src={user.backgroundImage || ""}
           alt={`Background image for ${user.name}`}
           height={1280}
           width={720}
           className="h-56 sm:h-96 sm:rounded-t-xl  w-screen"
         />
-        <label htmlFor="backgroundImage" className="absolute bottom-0 right-0 p-2 cursor-pointer">
+        <label
+          htmlFor="backgroundImage"
+          className="absolute bottom-0 right-0 p-2 cursor-pointer"
+        >
           <Icons.Image size={20} className="text-blue-400" />
         </label>
         <input
           id="backgroundImage"
           type="file"
           accept="image/*"
-          
           className="hidden"
         />
       </div>
