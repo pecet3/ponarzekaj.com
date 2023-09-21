8f28825114e48cb49d18e21d8baa4afe6d02aa88
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-21 22:41:19 +0200

    ui and logic

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -6,11 +6,13 @@ import { Icons } from "../../../components/ui/Icons";
 import { useSearchParams } from "next/navigation";
 import { RegisterForm } from "../../../components/auth/RegisterForm";
 import { LoginForm } from "../../../components/auth/LoginForm";
+import Link from "next/link";
 
-const page: React.FunctionComponent = ({}) => {
+const page = () => {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const searchParams = useSearchParams();
-  const firstTime = searchParams.get("first-time");
+  const register = searchParams.get("register");
+
   const error = searchParams.get("error");
   const loginWithGoogle = async () => {
     setIsLoading(true);
@@ -25,13 +27,13 @@ const page: React.FunctionComponent = ({}) => {
 
   return (
     <main className="flex flex-col background min-h-screen text-slate-800">
-      {!firstTime ? (
+      {!register ? (
         <div className="flex flex-col  mt-6 mx-auto bg-slate-400 p-2 rounded-md">
-          <h1 className="font-bold mb-2 text-xl">Zaloguj się za pomocą:</h1>
+          <h1 className="font-bold mb-2 text-xl">Zaloguj się</h1>
           <LoginForm />
           <button
             type="button"
-            className="rounded-md p-1 text-xl bg-blue-700 text-slate-300 font-semibold hover:bg-blue-600 duration-300 flex justify-center items-center"
+            className="rounded-md mt-2 p-1 text-xl bg-blue-700 text-slate-300 font-semibold hover:bg-blue-600 duration-300 flex justify-center items-center"
             onClick={loginWithGoogle}
             disabled={isLoading}
           >
@@ -44,12 +46,21 @@ const page: React.FunctionComponent = ({}) => {
               </span>
             )}
           </button>
+
+          <Link
+            href="/auth?register=true"
+            className="text-sm text-blue-800 underline text-center mt-2"
+          >
+            Zarejestruj się
+          </Link>
         </div>
       ) : (
-        <div className="flex flex-col justify-center items-center mt-6 mx-auto bg-slate-400 p-2 rounded-md">
+        <div className="flex flex-col  mt-6 mx-auto bg-slate-400 p-2 rounded-md">
           <h1 className="font-bold mb-2 text-xl">Zarejestruj się</h1>
           <RegisterForm />
-          <h2 className="my-2 text-base font-serif">lub wejdź inaczej...</h2>
+          <h2 className="my-2 text-base font-serif text-center">
+            lub wejdź inaczej...
+          </h2>
           <button
             type="button"
             className="rounded-md p-1 text-xl bg-blue-700 text-slate-300 font-semibold hover:bg-blue-600 duration-300 flex justify-center items-center"
@@ -65,8 +76,18 @@ const page: React.FunctionComponent = ({}) => {
               </span>
             )}
           </button>
+          <Link
+            href="/auth"
+            className="text-sm text-blue-800 underline text-center my-2"
+          >
+            Mam już konto
+          </Link>
         </div>
       )}
+      <p className="w-48 my-2 mx-auto text-xs text-center font-mono space-y-0">
+        Proszę nie zapominać haseł, jeszcze nie mamy podłączonego serwera email
+        do stronki
+      </p>
     </main>
   );
 };
