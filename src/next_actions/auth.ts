cde91b853b317b50499ab452557ad19625c71f1e
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-21 19:23:28 +0200

    logic

--- src/next_actions/auth.ts
+++ src/next_actions/auth.ts
@@ -4,30 +4,33 @@ import { revalidatePath } from "next/cache";
 import { db } from "@/lib/db";
 import { ratelimit } from "@/lib/redis";
 import { getAuthSession } from "@/lib/auth"
-import { authValidator } from '../lib/validators';
+import { registerValidator } from '../lib/validators';
 import bcrypt from "bcryptjs"
 export const register = async (form: FormData) => {
     try {
         const formEmail = form.getAll("email").toString()
         const formPassword = form.getAll("password").toString()
-
-        const { email, password } = authValidator.parse({ email: formEmail, password: formPassword })
+        const formName = form.getAll("name").toString()
+        const { email, password, name } = registerValidator.parse({ email: formEmail, password: formPassword, name: formName })
 
         const hashedPassword = await bcrypt.hash(password, 10)
+
         const isThatUser = await db.user.findMany({
             where: {
-                email,
-            }
+                email
+            },
         })
 
         if (isThatUser.length > 0) throw new Error()
 
         const user = await db.user.create({
             data: {
-                email: formEmail,
+                email,
                 password: hashedPassword,
+                name,
             }
         })
+        if (!user) throw new Error()
         return { success: true }
     } catch (error) {
         return { error }
