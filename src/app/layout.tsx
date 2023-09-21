c3ac2d118a1f865ddb5aca800939bd67902a4619
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-22 01:16:43 +0200

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Navbar } from "@/components/navbar/Navbar";
 import { Toaster } from "react-hot-toast";
 import Providers from "@/components/Providers";
 import { Icons } from "../components/ui/Icons";
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,9 +24,14 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
-          <footer className="bg-slate-700 text-slate-300 text-sm flex gap-4 items-center p-2 justify-center mt-8 ">
-            <div>
-              <p className="">Strona powstała w celu nauki oraz zabawy.</p>
+          <footer className="bg-slate-700 text-slate-300 text-sm flex gap-4 items-start p-2 justify-center mt-8 ">
+            <div className="flex gap-2 flex-col items-end ml-16">
+              <p className="font-mono max-w-md m-auto text-xs">
+                Cześć, bardzo mi miło, że odwiedziłeś moją stronę. Powstała ona
+                w celu dostarczenia dla Polaków miejsca, gdzie mogą robić, to co
+                lubią robić - narzekać. Strona cały czas się rozwija, także
+                zachęcam do śledzenia 😎.
+              </p>
               <p>
                 <a
                   href="https://github.com/pecet3"
@@ -36,16 +42,15 @@ export default function RootLayout({
                 , 2023
               </p>
             </div>
-            <a
+            <Link
               href="https://discord.gg/jDxBNk6M"
               className="text-center text-xs"
             >
               <Icons.Discord
-                size={32}
-                className="text-slate-50 hover:text-slate-300 duration-300 m-auto"
+                size={48}
+                className="text-slate-50 hover:text-blue-300 duration-300 m-auto"
               />
-              Nasz Discord
-            </a>
+            </Link>
           </footer>
         </Providers>
       </body>
