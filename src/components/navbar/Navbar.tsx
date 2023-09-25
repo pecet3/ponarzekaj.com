4969cdc946c3485358746b259152dc607467521e
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-25 21:43:57 +0200

    link to edit profile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -49,7 +49,7 @@ export const Navbar = async () => {
   return (
     <nav className="navbar bg-slate-700 text-slate-200 p-0.5">
       <div className="flex-1">
-        <Link className="" href="/">
+        <Link className="" href="/profile/edit">
           <img
             src="/logo.png"
             alt="logo"
