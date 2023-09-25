3ed0e7c0ed6aeabf6cbc5f227abd4d71097a27e8
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-25 21:43:50 +0200

    ui

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -7,6 +7,7 @@ import PaginationControls from "@/components/PaginationControls";
 import { MainTile } from "@/components/MainTile";
 import { Icons } from "@/components/ui/Icons";
 import { getAuthSession } from "@/lib/auth";
+import { SubmitButton } from "../../../components/SubmitButton";
 
 const page = async () => {
   const session = await getAuthSession();
@@ -37,22 +38,24 @@ const page = async () => {
           className="absolute rounded-full w-32 h-32  sm:w-40 sm:h-40 left-3 bottom-[-4rem] sm:bottom-[-5rem] "
         />
       </div>
-      <div className="flex flex-col sm:ml-48 ml-40 mt-2 text-slate-200">
+      <div className="flex flex-col sm:ml-48 ml-36 mt-2 text-slate-200 break-words">
         <span className="flex justify-between items-end">
           <p className="text-xl sm:text-2xl  font-bold">{user.name}</p>
         </span>
         <p className="text-base sm:text-xl ">{user.email}</p>
       </div>
-      <form className="mt-20">
+      <form className="mt-16 flex flex-col justify-center mx-auto gap-2 sm:bg-transparent bg-slate-700 rounded-md p-2">
         <input
           type="name"
           name="name"
           placeholder={user.name || "Wpisz swoje imię"}
+          className="rounded-md placeholder:p-1 bg-slate-400 placeholder:text-black"
         />
         <input
           type="email"
           name="email"
-          placeholder={user.name || "Wpisz swoje imię"}
+          placeholder={user.email || "Wpisz swoje imię"}
+          className="rounded-md placeholder:p-1 bg-slate-400 placeholder:text-black"
         />
       </form>
       <div className="opacity-0">.</div>
