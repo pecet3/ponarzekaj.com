7855a87aaada3c2e1627412ba8dca63c69da10f2
Jakub Pacewicz <pecet3107@gmail.coim>
2023-09-30 20:38:38 +0200

    new props

--- src/components/MainTile.tsx
+++ src/components/MainTile.tsx
@@ -3,12 +3,18 @@ import { Main } from "./Main";
 
 interface Props {
   children: React.ReactNode;
+  size?: string;
 }
 
-export const MainTile: FunctionComponent<Props> = ({ children }) => {
+export const MainTile: FunctionComponent<Props> = ({ children, size }) => {
   return (
     <Main>
-      <div className="w-full max-w-3xl flex flex-col md:rounded-xl bg-transparent sm:bg-slate-800 shadow-none sm:shadow-md sm:shadow-gray-700">
+      <div
+        className={`w-full ${
+          size ? size : "max-w-3xl"
+        } flex flex-col sm:rounded-xl bg-transparent sm:bg-slate-800 shadow-none sm:shadow-md sm:shadow-gray-700
+      `}
+      >
         {children}
       </div>
     </Main>
